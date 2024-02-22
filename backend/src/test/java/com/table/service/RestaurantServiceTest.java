805c2b9441e19168c2d69a0473d95f1a23c3e174
ZsoltEmber <132936073+ZsoltEmber@users.noreply.github.com>
2024-02-22 10:01:57 +0100

    add getRestaurants Unit test

--- backend/src/test/java/com/table/service/RestaurantServiceTest.java
+++ backend/src/test/java/com/table/service/RestaurantServiceTest.java
@@ -1,20 +1,43 @@
 package com.table.service;
 
+import com.table.controller.dto.RestaurantDTO;
 import com.table.model.Restaurant;
 import com.table.repository.TempRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.Set;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class RestaurantServiceTest {
+
+    private TempRepository tempRepositoryMock;
+    private RestaurantService restaurantService;
     @BeforeEach
-   public static RestaurantService restaurantService(){
-       return new RestaurantService(new TempRepository());
+    void beforeEach(){
+        tempRepositoryMock = mock(TempRepository.class);
+        restaurantService = new RestaurantService(tempRepositoryMock);
     }
 
     @Test
     void getRestaurants() {
+        Restaurant restaurant1 = new Restaurant(new RestaurantDTO("restaurant1", "fakeEmail", "admin", "555-555", "street street 15201") );
+        Restaurant restaurant2 = new Restaurant(new RestaurantDTO("restaurant2", "fakeEmail", "admin", "555-555", "street street 15201") );
+        when(tempRepositoryMock.getRestaurants()).thenReturn(Set.of(
+             restaurant1,
+             restaurant2
+        ));
+
+
+        Assertions.assertEquals(2, restaurantService.getRestaurants().size());
+        Assertions.assertTrue( restaurantService.getRestaurants().containsAll(List.of(restaurant1, restaurant2)));
     }
 
     @Test
