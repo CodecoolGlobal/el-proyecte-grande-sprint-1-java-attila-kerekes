4506259bf92e53268bac17f049e2ecafc7693bc1
MMate23 <132448311+MMate23@users.noreply.github.com>
2024-03-06 13:05:30 +0100

    Implement Lombok

--- backend/src/main/java/com/table/model/Reservation.java
+++ backend/src/main/java/com/table/model/Reservation.java
@@ -1,11 +1,16 @@
 package com.table.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.UUID;
-
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity
 public class Reservation {
     @Id
@@ -23,59 +28,5 @@ public class Reservation {
     @JoinColumn(name = "table_privateId", referencedColumnName = "privateId", nullable = false)
     private Table table;
 
-    public long getPrivateId() {
-        return privateId;
-    }
 
-    public UUID getPublicID() {
-        return publicID;
-    }
-
-    public LocalDateTime getStart() {
-        return start;
-    }
-
-    public Duration getDuration() {
-        return duration;
-    }
-
-    public int getNumberOfCustomers() {
-        return numberOfCustomers;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public Table getTable() {
-        return table;
-    }
-
-    public void setPrivateId(long privateId) {
-        this.privateId = privateId;
-    }
-
-    public void setPublicID(UUID publicID) {
-        this.publicID = publicID;
-    }
-
-    public void setStart(LocalDateTime start) {
-        this.start = start;
-    }
-
-    public void setDuration(Duration duration) {
-        this.duration = duration;
-    }
-
-    public void setNumberOfCustomers(int numberOfCustomers) {
-        this.numberOfCustomers = numberOfCustomers;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
-
-    public void setTable(Table table) {
-        this.table = table;
-    }
 }
