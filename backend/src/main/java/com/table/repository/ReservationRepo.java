c09ba0b4137b533c72f3564282d2fd406b26d345
ZsoltEmber <132936073+ZsoltEmber@users.noreply.github.com>
2024-03-08 08:23:37 +0100

    add necessary methods

--- backend/src/main/java/com/table/repository/ReservationRepo.java
+++ backend/src/main/java/com/table/repository/ReservationRepo.java
@@ -1,9 +1,14 @@
 package com.table.repository;
 
+import com.table.model.DiningSpot;
 import com.table.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Repository
 public interface ReservationRepo extends JpaRepository<Reservation, Long> {
+    List<Reservation> getReservationsByTable_PublicId(UUID uuid);
 }
--- backend/src/main/java/com/table/repository/RestaurantRepo.java
+++ backend/src/main/java/com/table/repository/RestaurantRepo.java
@@ -2,7 +2,6 @@ package com.table.repository;
 
 import com.table.model.Restaurant;
 import jakarta.transaction.Transactional;
-import org.hibernate.annotations.Cascade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,10 +13,16 @@ import java.util.UUID;
 @Repository
 public interface RestaurantRepo extends JpaRepository<Restaurant, Long> {
     Optional<Restaurant> findByEmailEqualsAndPasswordEquals(String email, String password);
+
     Optional<Restaurant> findByPublicId(UUID uuid);
+
     List<Restaurant> findAllByNameContainsIgnoreCase(String name);
+
+    List<Restaurant> findRestaurantsByNameRegex(String name);
+
     @Override
     List<Restaurant> findAll();
+
     @Transactional
     void deleteByPublicId(UUID uuid);
 
