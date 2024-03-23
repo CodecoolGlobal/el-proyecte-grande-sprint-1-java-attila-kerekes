856d66485042a10e6510fa6e4be52ee2bf1f6692
Attila Kerekes <133269554+attila-kerekes@users.noreply.github.com>
2024-03-23 17:12:00 +0100

    customer details and restaurant list listed in customer profile

---
+++ frontend/vite/src/pages/customer/CustomerDetails.jsx
@@ -0,0 +1,46 @@
+import {useEffect, useState} from "react";
+import {jwtDecode} from "jwt-decode";
+
+function CustomerDetails() {
+
+    const [customerDetails, setCustomerDetails] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const token = localStorage.getItem('token');
+                const decodedToken = jwtDecode(token);
+                const email = decodedToken.sub;
+                const response = await fetch(`/api/customers/email/${email}`, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    },
+                });
+
+                if (!response.ok) {
+                    throw new Error('Failed to fetch customer');
+                }
+
+                const customerData = await response.json();
+                setCustomerDetails(customerData);
+            } catch (error) {
+                console.error('Error fetching customer:', error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h3>Customer details</h3>
+            <div className="customerDetails" style={{border: "1px solid black"}} key={customerDetails.publicId}>
+                <h5>First name:{customerDetails.firstName}</h5>
+                <h5>Last name:{customerDetails.lastName}</h5>
+                <h5>E-mail: {customerDetails.email}</h5>
+                <h5>Phone: {customerDetails.phoneNumber}</h5>
+            </div>
+        </div>
+    )
+}
+
+export default CustomerDetails;
--- frontend/vite/src/pages/customer/CustomerRegistration.jsx
+++ frontend/vite/src/pages/customer/CustomerRegistration.jsx
@@ -1,4 +1,4 @@
-import CustomerRegistrationForm from "../components/customer_components/CustomerRegistrationForm.jsx";
+import CustomerRegistrationForm from "../../components/customer_components/CustomerRegistrationForm.jsx";
 
 export default function CustomerRegistration() {
     return (
