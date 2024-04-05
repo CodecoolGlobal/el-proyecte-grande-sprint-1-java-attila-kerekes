cc6105c5f0d279818daf753083a9a956c119cbcf
Attila Kerekes <133269554+attila-kerekes@users.noreply.github.com>
2024-04-05 07:42:00 +0200

    typo

--- frontend/vite/src/pages/restaurant/table/TableEditor.jsx
+++ frontend/vite/src/pages/restaurant/table/TableEditor.jsx
@@ -3,26 +3,26 @@ function TableEditor({ onSubmit, onChange }) {
     return (
         <div className={"tableEditorContainer"}>
             <h3>Add new table</h3>
-            <form style={{ width: "100%" }} onSubmit={(event) => onSubmit(event)}>
+            <form style={{width: "100%"}} onSubmit={(event) => onSubmit(event)}>
                 <div>
                     <input
-                        style={{ width: "90%" }}
+                        style={{width: "90%"}}
                         type={"text"}
                         name={"name"}
                         placeholder={"Table name"}
                         onChange={(event) => {
-                            onChange((prev) => ({ ...prev, name: event.target.value }));
+                            onChange((prev) => ({...prev, name: event.target.value}));
                         }}
                     />
                 </div>
                 <div>
                     <input
-                        style={{ width: "90%" }}
+                        style={{width: "90%"}}
                         type={"number"}
                         name={"seats"}
                         placeholder={"Number of seats"}
                         onChange={(event) => {
-                            onChange((prev) => ({ ...prev, capacity: parseInt(event.target.value) }));
+                            onChange((prev) => ({...prev, capacity: parseInt(event.target.value)}));
                         }}
                     />
                 </div>
